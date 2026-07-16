bf3f4bf569be3580fc5300edd9fcc73b3ea4ec37
AkramSaifi123 <akramsaifi3634@gmail.com>
2026-07-16 22:28:47 +0530

    todo context project

---
+++ 10_todoContextlocal/src/App.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [todos, setTodos] = useState([])
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [ {id : Date.now(), ...todo},...prev])
+  }
+
+  const updataTodo = (id, todo) => {
+    setTodos((prev) => prev.map((pravTodo) => pravTodo.id === id ? todo : pravTodo))
+  }
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id))
+  }
+
+  const toggalComplete = (id) => {
+    setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? { ...pravTodo, completed : !prevTodo.completed } : prevTodo))
+  }
+
+
+
+  return (
+    <>
+     <div className="bg-[#172842] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <div className="mb-4">
+                        {/* Todo form goes here */} 
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {/*Loop and Add TodoItem here */}
+                    </div>
+                </div>
+            </div>
+    </>
+  )
+}
+
+export default App
---
+++ 10_todoContextlocal/src/context/todoContext.js
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+
+
+export const todoContext = createContext({})
+
+export const todoProvider = todoContext.Provider
