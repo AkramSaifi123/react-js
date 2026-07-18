4eb7d336e6f4c50da692715a8ac5d93bac3606bc
AkramSaifi123 <akramsaifi3634@gmail.com>
2026-07-18 10:15:50 +0530

    components and context add

--- 10_todoContextlocal/src/context/todoContext.js
+++ 10_todoContextlocal/src/context/todoContext.js
@@ -1,6 +1,22 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 
 
-export const todoContext = createContext({})
+export const todoContext = createContext({
+    todos : [
+        {
+            id : 1,
+            todo : "todo msg",
+            completed : false
+        }
+    ], 
+    addTodo : (todo) => {},
+    updateTodo : (id, todo) => {},
+    deleteTodo : (id) => {},
+    toggleComplete : (id) => {}
+})
 
-export const todoProvider = todoContext.Provider
+export const useTodo = () => {
+    return useContext(todoContext)
+}
+
+export const TodoProvider = todoContext.Provider
